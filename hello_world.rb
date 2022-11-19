e3be10a44fdc614b47902564a2356818afc2a3e9
Natalia Voronjanska <voronjanskaja@gmail.com>
2022-11-19 19:14:42 +0200

    week1/2-exersise

---
+++ hello_world.rb
@@ -0,0 +1,7 @@
+def mult ()
+first_number = 3
+second_number = 10
+result = first_number*second_number
+print (result)
+end
+mult()
---
+++ week2.rb
@@ -0,0 +1,8 @@
+
+def get_volume_of_cuboid(length, width, height)
+  
+  result = length * width * height
+  puts result
+end
+get_volume_of_cuboid(23,3,3)
+
