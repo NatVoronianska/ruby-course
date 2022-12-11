3b040e04f60f5d724ec1057358d914e719508d80
Natalia Voronjanska <voronjanskaja@gmail.com>
2022-12-11 12:17:58 +0200

    our first class Hooray

--- week3.rb
+++ week3.rb
@@ -35,8 +35,110 @@ def my_metod(a)
       0
 end
 puts (my_metod(20))
-=end
+
 def print_welcome(name)
 print ("Hello, #{name}")
 end
 print_welcome("nat")
+
+def m1(proc)
+ proc.call 2
+end
+
+pr = proc { |n = 0| 3 * n }
+
+variable = m1(pr)
+puts variable
+
+def square_sum(numbers)
+  sum = 0
+  for num in numbers do
+    sum += num ** 2
+  end
+  sum
+end
+numbers = [1,9,7,0]
+puts square_sum(numbers)
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [1, 2, 3, 4, 5]
+puts arr1 == arr2
+
+arr3 = (1..5).to_a()
+
+print arr3
+
+arr1 = [5, 4, 3, 2, 1]
+arr3 = arr1.sort()
+print(arr1)
+puts(arr3)
+
+
+
+dict = {a: 50, b: 60}
+dict["b"] = 70
+print dict[:b]
+print dict.length
+
+def count_words(sentence)
+      sentence.split.length
+end
+puts (count_words("hello world or my best friend"))
+
+def count_repetitions(arr, el)
+    rep=0
+    for element in arr do
+       if element == el then 
+          rep+=1
+       end
+    end
+    return rep
+end
+puts(count_repetitions([1,1,5,2,2,3,3,5,5], 5))
+
+def matrix_params(arr)
+      m=u=o=0
+  for i in 0...arr.size do
+         for j in 0...arr.size do
+            if i==j
+                m+= arr[i][j]
+            elsif i>j
+                u+= arr[i][j]
+            else
+                o+= arr[i][j]
+            end
+         end
+         
+   end
+   return({"m"=>m, "u"=>u, "o"=>o})
+end
+puts(matrix_params([[1,2,3],[4,5,6],[7,8,9]]))
+
+re = /[A\- Z]/
+print re.match?("Some– World")
+=end
+class Pizza
+ @@count = 0
+  def initialize(d,ingredients)
+     @@count+=1
+     @number = @@count
+     @ingredients = ingredients
+     if d == 25 || d == 50 then
+      @d = d
+     else @d = 0
+      @number = 0
+      @@count = @@count -1
+      @ingredients = ""
+     end
+  end
+  def description
+    if @d == 25||@d ==50
+    print("Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients.join(",")}].")
+    else 
+      print("Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients}].")
+    end
+  end
+end
+pizza1 = Pizza.new(10,['mushroom', 'cheese'])
+pizza2 = Pizza.new(25,['mushroom', 'cheese', 'cherry'])
+pizza1.description 
+pizza2.description 
