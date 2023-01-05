4dc08b7c2a42e120970f9aba6ff1fb70f0260203
Natalia Voronjanska <voronjanskaja@gmail.com>
2023-01-05 22:19:45 +0200

    Update week3.rb

--- week3.rb
+++ week3.rb
@@ -115,38 +115,238 @@ puts(matrix_params([[1,2,3],[4,5,6],[7,8,9]]))
 
 re = /[A\- Z]/
 print re.match?("Some– World")
-=end
-class Pizza
- @@count = 0
- @@failinstance = 0
-  def initialize(d,ingredients)
-        @@count+=1
 
+class Pizza
+ attr_reader :d
+ @@count = 0
+  def initialize(d)
+     @@count+=1
      @number = @@count
-     @ingredients = ingredients
-     if d == 25 || d == 50 then
-      @d = d
-     else @d = 0
-      @number = 0
-      @ingredients = ""
-      @@count-=1
-      @@failinstance += 1
-     end
+     @ingredients = ''
+     @d = d
+   end
+  def ingredients=(ingredient)
+      a = ingredient.to_s
+      s = a.gsub(/[\',\", \[, \]]/,'')
+   @ingredients<<s
   end
   def description
-    if @d == 25||@d ==50
-    print("Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients.join(",")}].")
-    else 
-      print("Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients}].")
+     print("Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients}].")
     end
   end
-  def self.failInstance
-   @@failinstance
+  pizza1 = Pizza.new(24)
+puts pizza1.description 
+pizza1.ingredients = ["mushroom"]
+puts pizza1.description 
+class MyClass
+  attr_accessor :field
+
+  def to_s
+    @field
   end
-  
+
 end
-pizza1 = Pizza.new(24, ['mushroom', 'cheese'])
-pizza2 = Pizza.new(25, ['mushroom', 'cheese', 'cherry'])
-pizza3 = Pizza.new(20, ['mushroom', 'cheese'])
-pizza4 = Pizza.new(21, ['mushroom', 'cheese', 'cherry'])
-puts Pizza.failInstance
+
+ref = MyClass.new
+ref.field = 'Field'
+puts ref
+
+
+class Site
+  attr_accessor :current_url
+  HOME_URL = 'https://www.home.com'
+
+  def initialize
+    @home = HOME_URL
+    @current_url = HOME_URL
+  end
+
+   def go_to(date)
+    if @current_url == "#{@home}/#{date}"
+      nil
+    else
+    @current_url = "#{@home}/#{date}"
+    end
+  end
+
+  def to_s
+    "*----------*\n home: #{@home}\n current url: #{@current_url}\n*----------*"
+  end
+end
+a1 = Site.new
+a1.go_to("url1")
+puts a1
+p a1.go_to("url1")
+
+  class Site
+  attr_reader :current_url
+  HOME_URL = 'https://www.home.com'
+  @@path = []
+
+  def initialize
+    @home = HOME_URL
+    @current_url = HOME_URL
+     @@path << @current_url
+  end
+private
+ def self.correct
+  @@path.each do |x|
+    x.gsub!(" ","%20")
+    end
+  end
+
+public
+   def go_to(date)
+     
+    if @current_url == "#{@home}/#{date}"
+       Site.correct
+      nil
+    else
+    @current_url = "#{@home}/#{date}"
+     Site.correct
+       @@path << @current_url
+    end
+   end
+  def >(other)
+   if other.is_a? Site
+    @current_url > other.current_url
+   else 
+    false
+   end
+  end
+  def <(other)
+   if other.is_a? Site
+    @current_url < other.current_url
+   else 
+    false
+   end
+  end
+def ==(other)
+   if other.is_a? Site
+    @current_url == other.current_url
+   else 
+    false
+   end
+  end
+  def to_s
+    "*----------*\n home: #{@home}\n current url: #{@current_url}\n history: #{@@path}\n*----------*"
+
+  end
+ end
+ a1 = Site.new
+a1.go_to("first")
+a2 = Site.new
+a2.go_to("second")
+a1.go_to("first 1")
+a1.go_to("first 2")
+a1.go_to("first 2")
+puts a1
+
+class Parent
+  private
+  def method1
+    'abc'
+  end
+end
+
+class Child < Parent
+  def method1
+    super
+  end
+end
+
+child = Child.new
+puts child.method1
+
+ module ConsoleInput 
+  def string
+   puts ('Enter any string')
+   str = gets.strip.to_s
+  end
+  def number
+  puts ('Enter any number')
+   num = gets.strip
+   if num.kind_of? Integer 
+     returne num.to_i
+   elsif num.kind_of? Float 
+     returne num.to_f
+  end
+  def symbol 
+   puts('Enter any letters')
+   s = gets.strip.to_sym
+  end
+  def hash
+   puts("Enter hash in format 'key: value' or 'key => value'")
+   str = gets.strip
+   
+   puts str
+  end
+ end
+end
+sqrt(4) => 2.0
+sqrt(5) => 2.2361
+sqrt(-3) => виключення StandardError з повідомленням "The root of a negative number does not exist in rational numbers"
+
+class PhoneFormatException < StandardError
+  def initialize(user_phone = nil)
+    @ph = user_phone
+  end
+
+  def to_s
+    "Phone number #{@ph} is incorrect. You should enter phone in format +38dddddddddd"
+  end
+end
+
+  module Validation 
+    def phone_valid?(user_phone)
+      if user_phone.match?(/^+38dddddddddd$/)
+      end
+    end
+  end    
+
+class User
+  include Validation 
+  attr_reader :name
+  attr_writer :phone
+  
+  def initialize(name)
+    @name = name
+  end  
+  def phone=(user_phone)
+    if phone_valid?(user_phone)
+      @phone = user_phone
+    else
+      raise PhoneFormatException.new(user_phone)
+    end
+  end
+end
+
+
+  user = User.new('Name')
+  user.phone = '806787655678888'
+  puts method(:phone_valid?).bind(self).call('+380876756543') 
+=end
+def third_order(num)
+ if num==1||num==0 
+  n=1
+ else 
+  n = rand(num+1)
+ end 
+ third = n**3
+ count = 0
+ begin
+  count+=1
+  puts("What is result #{n} * #{n} * #{n}?")
+  int = gets.strip.to_i
+  if third == int 
+    puts ('Good job')
+    return
+  end
+  raise
+  rescue 
+  if count<3 
+   retry
+  else puts'Try next time' 
+  end
+ end
+end
